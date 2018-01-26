92877058df5d7a9157a88bc8dbff7ad0edc51d98
Daniel Afonso <Daniel-Af@outlook.com>
2018-01-26 16:00:43 +0000

    Aria

--- js/utiles.js
+++ js/utiles.js
@@ -1,9 +1,9 @@
 let cardTitle =
     "<div tabindex='-1' class='cardTitle card text-center' style='width: 30%; min-width: 18rem; margin-top: 2rem;'>" +
-    "<img aria-hidden='true' class='card-img-top' src='recursos/caratulas/avatar.jpg' alt='Card image cap'>" +
+    "<img aria-label='Imagen de cartelera de la película.' class='card-img-top' src='recursos/caratulas/avatar.jpg' alt='Card image cap'>" +
     "<div class='card-body'> " +
     " <h1 tabindex='0' class='card-title'></h1>" +
-    " <p  tabindex='0' class='card-text'></p>" +
+    " <p tabindex='0' class='card-text'></p>" +
     "<button type='button' href='#' aria-label='Pulse enter para ver más información.' tabindex='0' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Información</button> " +
     "</div>" +
     "</div>";
@@ -36,4 +36,4 @@ var tipos = [{
 }, {
     title: 'Resultados de la votación:',
     pieHole: 0.4
-} ]
+}]
